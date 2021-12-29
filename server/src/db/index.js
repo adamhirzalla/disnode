23cf9d65d2f5c4f7fd8f29f4126a3d94df533278
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-29 01:06:39 -0500

    import and use chalk for coloring

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,4 +1,5 @@
 const { Client } = require("pg");
+const chalk = require("chalk");
 
 // PG database client/connection setup
 const client = new Client({
@@ -8,16 +9,19 @@ const client = new Client({
 
 client
   .connect()
+  .then(() =>
+    console.log(
+      chalk.cyan(
+        `-> Connected to ${process.env.PGDATABASE} database on Port ${process.env.PGPORT}`
+      )
+    )
+  )
   .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
-
-console.log(
-  `Connected to Postgres server ${process.env.PGDATABASE} on Port ${process.env.PGPORT}`
-);
 
 module.exports = {
   query: (text, params, callback) => {
-    console.log("Query:", text);
-    console.log("Values:", params);
+    // console.log("Query:", text);
+    // console.log("Values:", params);
     return client.query(text, params, callback);
   },
 };
