066ed5e539b5a70c97178047725f1e9de156b874
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-30 02:45:11 -0500

    put await logic inside try/catch

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -7,12 +7,16 @@ const Users = require("../db/queries/users");
 // Testing token
 router.post("/me", auth, async (req, res) => {
   const userId = req.user.id;
-  const user = await Users.byId(userId);
-  res.send(`Authenticated as: ${user.full_name}`);
+  try {
+    const user = await Users.byId(userId);
+    res.status(200).send(`Authenticated as: ${user.full_name}`);
+  } catch (e) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // get all users
-router.get("/users", auth, async (req, res) => {
+router.get("/users", (req, res) => {
   Users.test().then((result) => res.json(result));
 });
 module.exports = router;
