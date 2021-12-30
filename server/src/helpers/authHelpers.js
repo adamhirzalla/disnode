dd79442de3c46dbfea67cc8d4e85a22b60fbf939
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-29 23:48:45 -0500

    add joi validation for login & register inputs

--- server/src/helpers/authHelpers.js
+++ server/src/helpers/authHelpers.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const jwt = require("jsonwebtoken");
 
 const accessKey = process.env.JWT_ACCESS_KEY;
@@ -15,4 +16,37 @@ const generateRefresh = (id) => {
   });
 };
 
-module.exports = { generateAccess, generateRefresh };
+/* Input validation for user login */
+const validateRegister = (input) => {
+  const schema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    full_name: Joi.string().min(3).max(40).required(),
+    display_name: Joi.string().min(3).max(40).required(),
+    email: Joi.string().min(5).max(254).required().email(),
+    password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required(),
+    repeat_password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required(),
+  }).with("password", "repeat_password");
+  return schema.validate(input);
+};
+
+/* Input validation for user login */
+const validateLogin = (input) => {
+  const schema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required(),
+  });
+  return schema.validate(input);
+};
+
+module.exports = {
+  generateAccess,
+  generateRefresh,
+  validateLogin,
+  validateRegister,
+};
