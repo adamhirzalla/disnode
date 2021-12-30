1831269a3c7481c70854cfdea02b49bfb1fe1ec6
EavanK <ksoksl0711@gmail.com>
2021-12-30 14:00:44 -0800

    modify test users table url

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./auth/Login";
+import "./App.css";
+import Register from "./auth/Register";
+import Nav from "./auth/Nav";
+import Home from "./auth/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route element={<Home />} path="/" exact="true" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Register />} path="/register" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
