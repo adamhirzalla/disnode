87b92c7620ead123de229948daaba2f52f368f7b
EavanK <ksoksl0711@gmail.com>
2021-12-31 02:38:19 -0800

    implement regitsterUser event handler

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -13,16 +13,42 @@ export const AuthProvider = ({ children }) => {
   const [authTokens, setAuthTokens] = useState(initialTokens);
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState({});
-
   const navigate = useNavigate();
 
+  // Register a user
+  const registerUser = async (e) => {
+    e.preventDefault();
+    const userInfo = {
+      full_name: e.target.full_name.value,
+      display_name: e.target.display_name.value,
+      username: e.target.username.value,
+      email: e.target.username.value,
+      password: e.target.password.value,
+      repeat_password: e.target.confirm_password.value,
+    };
+    // Check if forms are filled in
+    if (Object.values(userInfo).includes("")) {
+      return alert("Please fill in all the forms");
+    }
+    try {
+      const res = await axios.post("/api/register", userInfo);
+      const { tokens, user } = res.data;
+      setAuthTokens((prev) => ({ ...prev, ...tokens }));
+      setUser((prev) => ({ ...prev, ...user }));
+      localStorage.setItem("authTokens", JSON.stringify(tokens));
+      navigate("/");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // Login a user
   const loginUser = async (e) => {
     const userInfo = {
       username: e.target.username.value,
       password: e.target.password.value,
     };
     e.preventDefault();
-
     try {
       const res = await axios.post("/api/login", userInfo);
       const { tokens, user } = res.data;
@@ -35,6 +61,7 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
+  // Logout a user
   const logoutUser = () => {
     setAuthTokens(null);
     setUser(null);
@@ -42,32 +69,38 @@ export const AuthProvider = ({ children }) => {
     navigate("/login");
   };
 
+  // Get a user information
   const getUser = async (key) => {
     try {
       const headers = {
         Authorization: `Bearer ${key}`,
       };
-      const response = await axios.get("/api/me", { headers });
-      const user = response.data;
+      const res = await axios.get("/api/me", { headers });
+      const user = res.data;
       setUser((prev) => ({ ...prev, ...user }));
     } catch (e) {
       console.log(e);
     }
   };
 
+  // Update tokens
   const updateTokens = async () => {
-    try {
-      const refreshToken = JSON.parse(
-        localStorage.getItem("authTokens")
-      ).refreshToken;
-      const response = await axios.post("/api/token", { refreshToken });
-      const updatedTokens = await response.data;
-      localStorage.setItem("authTokens", JSON.stringify(updatedTokens));
-      setAuthTokens((prev) => ({ ...prev, ...updatedTokens }));
-      getUser(updatedTokens.accessToken);
-    } catch (e) {
-      console.log(e);
+    if (localStorage.getItem("authTokens")) {
+      try {
+        const refreshToken = JSON.parse(
+          localStorage.getItem("authTokens")
+        ).refreshToken;
+        const res = await axios.post("/api/token", { refreshToken });
+        const updatedTokens = await res.data;
+        localStorage.setItem("authTokens", JSON.stringify(updatedTokens));
+        setAuthTokens((prev) => ({ ...prev, ...updatedTokens }));
+        getUser(updatedTokens.accessToken);
+      } catch (e) {
+        console.log(e);
+      }
+      return setLoading(false);
     }
+    logoutUser();
     setLoading(false);
   };
 
@@ -79,13 +112,14 @@ export const AuthProvider = ({ children }) => {
     return () => clearInterval(refreshToken);
   }, []);
 
+  // send data/function to children components
   const contextData = {
     user,
     authTokens,
     loginUser,
     logoutUser,
+    registerUser,
   };
-
   return (
     <AuthContext.Provider value={contextData}>
       {loading ? null : children}
