a07984e9c526c7f81987b221b09dd994c089f57b
Hang <jsu6898@gmail.com>
2021-12-30 19:53:28 -0800

    react skeleton wip

---
+++ client/src/components/AvatarButton/UserAvatarButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+export default function BasicButtons(props) {
+  return (
+    <>
+      <Button variant="contained">Contained</Button>
+    </>
+  );
+}
---
+++ client/src/components/Navbar/ServerBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from '@mui/material';
+
+export default function ServerBar() {
+  return (
+    <div className='ServerBar'>
+      <Typography variant="h6">Hello</Typography>
+    </div>
+  )
+}
