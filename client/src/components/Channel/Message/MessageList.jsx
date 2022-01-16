dcf661586bcfab61ab2e646ce13605f56796da84
EavanK <eavan.hsk@gmail.com>
2022-01-15 19:45:09 -0800

    move mockdata from MessageListItem to MessageList component

--- client/src/components/Channel/Message/MessageList.jsx
+++ client/src/components/Channel/Message/MessageList.jsx
@@ -13,7 +13,109 @@ import { AddCircle, Send } from "@mui/icons-material";
 import MessageListItem from "./MessageListItem";
 import { useMessageListSytle } from "../../styles/useMessageListSytle";
 
-export default function MessageList({ children }) {
+const mockMessages = [
+  {
+    id: 1,
+    user_id: 3,
+    name: "Hyunsu",
+    img: "https://photolemur.com/uploads/blog/unnamed.jpg",
+    msg: "Hello guys",
+  },
+  {
+    id: 2,
+    user_id: 1,
+    name: "Adam",
+    img: "https://images.unsplash.com/photo-1556103255-4443dbae8e5a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG9ncmFwaGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    msg: "Sup",
+  },
+  {
+    id: 3,
+    user_id: 2,
+    name: "Jonathan",
+    img: "https://static8.depositphotos.com/1008939/939/i/600/depositphotos_9394698-stock-photo-lonely-man.jpg",
+    msg: "How's it going",
+  },
+  {
+    id: 4,
+    user_id: 5,
+    name: "Jono",
+    img: "https://wl-brightside.cf.tsp.li/resize/728x/jpg/6f5/d79/6c2d4457e7b227254fbc0f51b8.jpg",
+    msg: "wanna play?",
+  },
+  { id: 5, user_id: 6, name: "Lulu", img: "", msg: "I can play tomorrow" },
+  {
+    id: 6,
+    user_id: 7,
+    name: "Lala",
+    img: "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG98ZW58MHx8MHx8&w=1000&q=80",
+    msg: "I can play now invite me",
+  },
+  {
+    id: 7,
+    user_id: 4,
+    name: "Ted",
+    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
+    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
+  },
+  {
+    id: 8,
+    user_id: 4,
+    name: "Ted",
+    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
+    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
+  },
+  {
+    id: 9,
+    user_id: 4,
+    name: "Ted",
+    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
+    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
+  },
+  {
+    id: 10,
+    user_id: 4,
+    name: "Ted",
+    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
+    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
+  },
+  {
+    id: 11,
+    user_id: 4,
+    name: "Ted",
+    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
+    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
+  },
+  {
+    id: 12,
+    user_id: 4,
+    name: "Ted",
+    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
+    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
+  },
+  {
+    id: 13,
+    user_id: 4,
+    name: "Ted",
+    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
+    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
+  },
+  {
+    id: 14,
+    user_id: 4,
+    name: "Ted",
+    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
+    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
+  },
+  {
+    id: 15,
+    user_id: 4,
+    name: "Ted",
+    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
+    msg: "hahahhahaahahhahahhahahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhahahhahahhahahhahahhahahhahahhahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
+  },
+];
+
+export default function MessageList({ children, messages }) {
   const classes = useMessageListSytle();
   const [message, setMessage] = React.useState("");
 
@@ -25,6 +127,16 @@ export default function MessageList({ children }) {
   const handleSendButtonClick = (e) => {
     console.log(e.target.value);
   };
+  messages = mockMessages;
+  const messageItems = messages.map((user) => {
+    return (
+      <MessageListItem
+        key={user.id}
+        user={user}
+        onClick={handleSendButtonClick}
+      />
+    );
+  });
 
   return (
     <>
@@ -44,9 +156,7 @@ export default function MessageList({ children }) {
         </ListItem>
         <Divider />
 
-        <Box className={classes.message}>
-          <MessageListItem onClick={handleSendButtonClick} />
-        </Box>
+        <Box className={classes.message}>{messageItems}</Box>
         <Divider />
 
         <Box sx={{ display: "flex", justifyContent: "center" }}>
--- client/src/components/Channel/Message/MessageListItem.jsx
+++ client/src/components/Channel/Message/MessageListItem.jsx
@@ -2,122 +2,32 @@ import * as React from "react";
 import { Box, Avatar, Typography, Grid, List } from "@mui/material";
 import { useMessageListSytle } from "../../styles/useMessageListSytle";
 
-const mockMessages = [
-  {
-    id: 3,
-    name: "Hyunsu",
-    img: "https://photolemur.com/uploads/blog/unnamed.jpg",
-    msg: "Hello guys",
-  },
-  {
-    id: 1,
-    name: "Adam",
-    img: "https://images.unsplash.com/photo-1556103255-4443dbae8e5a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG9ncmFwaGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-    msg: "Sup",
-  },
-  {
-    id: 2,
-    name: "Jonathan",
-    img: "https://static8.depositphotos.com/1008939/939/i/600/depositphotos_9394698-stock-photo-lonely-man.jpg",
-    msg: "How's it going",
-  },
-  {
-    id: 5,
-    name: "Jono",
-    img: "https://wl-brightside.cf.tsp.li/resize/728x/jpg/6f5/d79/6c2d4457e7b227254fbc0f51b8.jpg",
-    msg: "wanna play?",
-  },
-  { id: 6, name: "Lulu", img: "", msg: "I can play tomorrow" },
-  {
-    id: 7,
-    name: "Lala",
-    img: "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG98ZW58MHx8MHx8&w=1000&q=80",
-    msg: "I can play now invite me",
-  },
-  {
-    id: 4,
-    name: "Ted",
-    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
-    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
-  },
-  {
-    id: 4,
-    name: "Ted",
-    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
-    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
-  },
-  {
-    id: 4,
-    name: "Ted",
-    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
-    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
-  },
-  {
-    id: 4,
-    name: "Ted",
-    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
-    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
-  },
-  {
-    id: 4,
-    name: "Ted",
-    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
-    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
-  },
-  {
-    id: 4,
-    name: "Ted",
-    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
-    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
-  },
-  {
-    id: 4,
-    name: "Ted",
-    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
-    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
-  },
-  {
-    id: 4,
-    name: "Ted",
-    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
-    msg: "hahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
-  },
-  {
-    id: 4,
-    name: "Ted",
-    img: "https://i1.sndcdn.com/artworks-HgiHqHrCBnVFJmok-s39fqQ-t500x500.jpg",
-    msg: "hahahhahaahahhahahhahahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhhahhahahhahahhahahhahahhahahhahahhahahhahahhahahhahahhahahhahahhahahhahaahahhahahhahaahahhahahhahaahahhahahhahaahah",
-  },
-];
-
-export default function MessageListItem({ onClick }) {
+export default function MessageListItem({ onClick, user }) {
   const classes = useMessageListSytle();
 
-  return mockMessages.map((user, i) => {
-    return (
-      <Grid key={i} container alignItems="center" sx={{ height: 400 }}>
-        <Grid xs={1.2} item sx={{ height: "80%" }}>
-          <Box className={classes.avatar}>
-            <Avatar
-              alt={user.name}
-              src={user.img}
-              sx={{ width: 40, height: 40, mt: 1 }}
-            />
-            <Typography>{user.name}</Typography>
-          </Box>
-        </Grid>
-        <Grid xs={"auto"} item>
-          <List id="chat-windows-messages" sx={{ maxWidth: 700 }}>
-            <Typography
-              className={classes.messages}
-              variant="body1"
-              display="block"
-            >
-              {user.msg}
-            </Typography>
-          </List>
-        </Grid>
+  return (
+    <Grid container alignItems="center" sx={{ height: 400 }}>
+      <Grid xs={1.2} item sx={{ height: "80%" }}>
+        <Box className={classes.avatar}>
+          <Avatar
+            alt={user.name}
+            src={user.img}
+            sx={{ width: 40, height: 40, mt: 1 }}
+          />
+          <Typography>{user.name}</Typography>
+        </Box>
       </Grid>
-    );
-  });
+      <Grid xs={"auto"} item>
+        <List id="chat-windows-messages" sx={{ maxWidth: 700 }}>
+          <Typography
+            className={classes.messages}
+            variant="body1"
+            display="block"
+          >
+            {user.msg}
+          </Typography>
+        </List>
+      </Grid>
+    </Grid>
+  );
 }
