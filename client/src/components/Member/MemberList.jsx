8aee8e039bbfea162e74e1e9b904aae9c5961e51
EavanK <eavan.hsk@gmail.com>
2022-01-18 02:17:22 -0800

    refactor MemberList styles

--- client/src/components/Member/MemberList.jsx
+++ client/src/components/Member/MemberList.jsx
@@ -1,69 +1,21 @@
 import { useContext, useState } from "react";
 import { styled } from "@mui/material/styles";
-import MuiDrawer from "@mui/material/Drawer";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import { Box, IconButton, CssBaseline } from "@mui/material";
-import MemberListItem from "./MemberListItem";
+import { ChevronRight, ChevronLeft } from "@mui/icons-material";
+import { Box, IconButton, Drawer } from "@mui/material";
 import { useMemberListStyles } from "../styles/useMemberListStyles";
 import ServerContext from "../../contexts/ServerContext";
+import MemberListItem from "./MemberListItem";
+import classNames from "classnames";
 
-const drawerWidth = "250px";
-
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(10)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(15)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open
-    ? {
-        ...openedMixin(theme),
-        "& .MuiDrawer-paper": openedMixin(theme),
-      }
-    : {
-        ...closedMixin(theme),
-        "& .MuiDrawer-paper": closedMixin(theme),
-      }),
-}));
-
-export default function MemberList({ socket, children }) {
+export default function MemberList({ socket }) {
   const classes = useMemberListStyles();
   const [open, setOpen] = useState(false);
   const {
     app: { members },
   } = useContext(ServerContext);
+  const drawerClass = classNames(classes.drawer, {
+    [classes.drawerOpen]: open,
+  });
 
   const handleDrawerOpen = () => {
     socket.emit("get online");
@@ -85,26 +37,21 @@ export default function MemberList({ socket, children }) {
   });
 
   return (
-    <>
-      <Box sx={{ position: "sticky" }}>
-        <CssBaseline />
-        <Drawer variant="permanent" anchor="right" open={open}>
-          {memberList}
-          {/* {open && (
-            <Box sx={{ position: "fixed", bottom: 0, right: 250 }}>
-              <IconButton onClick={open ? handleDrawerClose : handleDrawerOpen}>
-                <ChevronRightIcon />
-              </IconButton>
-            </Box>
-          )} */}
-          <DrawerHeader sx={{ position: "fixed", bottom: 0, right: 0 }}>
-            <IconButton onClick={open ? handleDrawerClose : handleDrawerOpen}>
-              {open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-            </IconButton>
-          </DrawerHeader>
-        </Drawer>
-      </Box>
-      {children}
-    </>
+    <Box sx={{ position: "sticky" }}>
+      <Drawer
+        className={drawerClass}
+        variant="permanent"
+        anchor="right"
+        open={open}
+      >
+        {memberList}
+        <IconButton
+          className={classes.closeIcon}
+          onClick={open ? handleDrawerClose : handleDrawerOpen}
+        >
+          {open ? <ChevronRight /> : <ChevronLeft />}
+        </IconButton>
+      </Drawer>
+    </Box>
   );
 }
