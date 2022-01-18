ea946d8e68ed59bccd682bfc30f0fb77599b46b5
Hang <jsu6898@gmail.com>
2022-01-17 21:22:58 -0800

    changes all DisComponents to take prop:type

--- client/src/components/Button/DisIconButton.jsx
+++ client/src/components/Button/DisIconButton.jsx
@@ -5,12 +5,12 @@ import classNames from "classnames";
 import { useDisIconButtonStyles } from "../styles/useDisIconButtonStyles";
 
 export default function DisIconButton(props) {
-  const { home, children, ...rest } = props;
+  const { type, children, ...rest } = props;
 
   const classes = useDisIconButtonStyles();
 
   const iconClass = classNames(classes.root, {
-    [classes.home]: home,
+    [classes.home]: type === "home",
   });
 
   return (
