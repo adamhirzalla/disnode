93d6c787dfb5cb34216142adfaf15a14897b720b
Adam Hirzalla <adam.hirzalla@carleton.ca>
2022-01-17 21:15:24 -0500

    test font change for theme

--- client/src/themes/theme.jsx
+++ client/src/themes/theme.jsx
@@ -1,18 +1,6 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-  components: {
-    MuiBox: {
-      styleOverrides: {
-        root: {
-          width: "100%",
-        },
-      },
-    },
-  },
-});
-
-export const darkTheme = createTheme({
   // components: {
   //   MuiBox: {
   //     styleOverrides: {
@@ -22,6 +10,23 @@ export const darkTheme = createTheme({
   //     },
   //   },
   // },
+  typography: {
+    fontFamily: "Roboto Mono",
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 600,
+  },
+});
+
+export const darkTheme = createTheme({
+  typography: {
+    fontFamily: "Roboto Mono",
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 600,
+  },
   palette: {
     mode: "dark",
   },
