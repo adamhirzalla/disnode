0e58d50c63f1011484acc2e2c1cfa943656b607d
Adam Hirzalla <adam.hirzalla@carleton.ca>
2022-01-19 19:41:11 -0500

    slight refactor + testing Skeleton components

--- client/src/components/Server/ServerList.jsx
+++ client/src/components/Server/ServerList.jsx
@@ -8,7 +8,14 @@ import NewServerDialog from "./NewServerDialog";
 import DisDivider from "../../Divider/DisDivider";
 import ServerContext from "../../contexts/ServerContext";
 import SearchServerDialog from "./SearchServerDialog";
-import { List, IconButton, Tooltip, ListItem, Avatar } from "@mui/material";
+import {
+  List,
+  IconButton,
+  Tooltip,
+  ListItem,
+  Avatar,
+  Skeleton,
+} from "@mui/material";
 import { useDisIconButtonStyles } from "../styles/useDisIconButtonStyles";
 import {
   createServer,
@@ -50,24 +57,36 @@ const useStyles = makeStyles(() => ({
 
 export default function ServerList(props) {
   const { socket, user, children } = props;
-
   const {
     app: { servers, mode, loading },
     setServer,
     setServers,
     setMode,
   } = useContext(ServerContext);
-
-  const parsedServers = servers.map((server) => {
-    return (
-      <ServerListItem
-        key={server.id}
-        id={server.id}
-        title={server.title}
-        logo={server.logo}
-      />
-    );
+  const iconClasses = useDisIconButtonStyles();
+  const classes = useStyles();
+  const homeIconClass = classNames(iconClasses.home, {
+    [iconClasses.selected]: mode === HOME,
   });
+
+  // : [...new Array(7)].map((e, i) => {
+  //     return (
+  //       <ServerListItem key={i} logo={"/images/Disnode.png"}>
+  //         <Skeleton
+  //           sx={{ bgcolor: "#B13737", opacity: "1" }}
+  //           variant="circular"
+  //           width={"65px"}
+  //           height={"65px"}
+  //           animate={"wave"}
+  //         />
+  //       </ServerListItem>
+  //     );
+  //   });
+
+  const handleHomeClick = (socket) => {
+    socket.emit("home click", socket.id, user.display_name);
+    setMode(HOME);
+  };
 
   const handleCreate = async (input) => {
     const { title, tags, file } = input;
@@ -84,22 +103,23 @@ export default function ServerList(props) {
     setServer(server);
   };
 
-  const handleHomeClick = (socket) => {
-    socket.emit("home click", socket.id, user.display_name);
-    setMode(HOME);
-  };
-
   // useEffect(() => {
   //   socket?.emit("connection", socket.id, user.display_name);
   // }, []);
-  const iconClasses = useDisIconButtonStyles();
-  const classes = useStyles();
-  const homeIconClass = classNames(iconClasses.home, {
-    [iconClasses.selected]: mode === HOME,
+
+  const parsedServers = servers.map((server) => {
+    return (
+      <ServerListItem
+        key={server.id}
+        id={server.id}
+        title={server.title}
+        logo={server.logo}
+      />
+    );
   });
+
   return (
     <DisBox disStyle="navBox">
-      {/* <CssBaseline /> */}
       <DisDrawer disStyle="nav" variant="permanent" anchor="left">
         <List>
           <Tooltip
@@ -144,86 +164,3 @@ export default function ServerList(props) {
     </DisBox>
   );
 }
-
-//old component
-
-// import Box from "@mui/material/Box";
-// import List from "@mui/material/List";
-// import Drawer from "@mui/material/Drawer";
-// import Divider from "@mui/material/Divider";
-// import { useContext, useEffect } from "react";
-// import ServerListItem from "./ServerListItem";
-// import NewServerDialog from "./NewServerDialog";
-// import IconButton from "@mui/material/IconButton";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import ServerContext from "../../contexts/ServerContext";
-// import { useServerListStyles } from "../styles/useServerListStyles";
-// import {
-//   createServer,
-//   createTags,
-//   getServer,
-//   getServers,
-// } from "../../network/serverApi";
-
-// export default function ServerList(props) {
-//   const classes = useServerListStyles();
-//   const { socket, user, children } = props;
-
-//   const {
-//     app: { servers },
-//     setServer,
-//     setServers,
-//   } = useContext(ServerContext);
-
-//   const parsedServers = servers.map((server) => {
-//     return (
-//       <ServerListItem
-//         key={server.id}
-//         server={server.id}
-//         id={server.id}
-//         title={server.title}
-//         logo={server.logo}
-//         setServer={setServer}
-//       />
-//     );
-//   });
-
-//   // experimenting adding server
-//   const addServer = async (input) => {
-//     const { title, tags, logo } = input;
-//     const { id } = await createServer(title, logo);
-//     const servers = await getServers();
-//     const server = await getServer(id);
-//     setServers(servers);
-//     setServer(server);
-//     await createTags(tags, server.id);
-//   };
-
-//   const handleHomeClick = (socket) => {
-//     socket.emit("home click", socket.id, user.display_name);
-//   };
-
-//   useEffect(() => {
-//     socket?.emit("connection", socket.id, user.display_name);
-//   }, []);
-
-//   return (
-//     <Box className={classes.box}>
-//       <CssBaseline />
-//       <Drawer className={classes.serverList} variant="permanent" anchor="left">
-//         <IconButton type="home" onClick={() => handleHomeClick(socket)}>
-//           <img alt="Home" src="/images/Disnode-red.png" width="70px" />
-//         </IconButton>
-//         <Divider className={classes.divider} />
-//         <Box ml={"auto"} mr={"auto"}>
-//           <List>{parsedServers}</List>
-//         </Box>
-//         <Divider />
-//         <Box ml={"auto"} mr={"auto"}>
-//           <NewServerDialog onClick={addServer} />
-//         </Box>
-//       </Drawer>
-//       {children}
-//     </Box>
-//   );
-// }
