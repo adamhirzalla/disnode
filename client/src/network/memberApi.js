c4096ac3be8d359c257fac9c27c7fa55cbc9512d
Hang <jsu6898@gmail.com>
2022-01-21 23:54:20 -0800

    leave server working

--- client/src/network/memberApi.js
+++ client/src/network/memberApi.js
@@ -11,3 +11,14 @@ export const updateRole = async (serverId, memberId, role) => {
     console.log("Failed to retreive message data", e);
   }
 };
+
+export const removeMember = async (serverId, memberId) => {
+  try {
+    const res = await axios.delete(
+      `/api/servers/${serverId}/members/${memberId}`
+    );
+    return res.data;
+  } catch (e) {
+    console.log("Failed to retreive message data", e);
+  }
+};
