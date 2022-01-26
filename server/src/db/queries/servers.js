0cfa91402c3799b03e3fde7614aa9f3a91a43014
Adam Hirzalla <adam.hirzalla@carleton.ca>
2022-01-25 22:04:09 -0500

    add servers by tags

--- server/src/db/queries/servers.js
+++ server/src/db/queries/servers.js
@@ -88,6 +88,38 @@ const byTitle = (title) => {
     });
 };
 
+const byTags = (tags) => {
+  const query = `
+  SELECT DISTINCT
+    servers.id,
+    title,
+    logo,
+    creator_id AS owner_id
+  FROM servers
+  JOIN server_tags ON servers.id = server_id
+  JOIN tags ON tags.id = tag_id
+  WHERE tag_id = ANY(ARRAY[${tags}]);
+  `;
+  return db
+    .query(query)
+    .then((res) => res.rows)
+    .then((servers) => {
+      const membersQueries = servers.map((server) =>
+        Member.byServer(server.id)
+      );
+      const tagsQueries = servers.map((server) => Tag.byServer(server.id));
+      return Promise.all(membersQueries).then((members) => {
+        return Promise.all(tagsQueries).then((tags) => {
+          servers.forEach((server, i) => {
+            server.members = members[i];
+            server.tags = tags[i];
+          });
+          return servers;
+        });
+      });
+    });
+};
+
 const byCode = (code) => {
   const query = `
   SELECT 
@@ -161,6 +193,7 @@ module.exports = {
   byUser,
   byTitle,
   byCode,
+  byTags,
   byID,
   create,
   createTags,
